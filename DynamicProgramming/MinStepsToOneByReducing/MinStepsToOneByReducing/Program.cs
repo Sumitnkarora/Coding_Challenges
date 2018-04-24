b25176ab4e5ccf8e35137db2d16dea511f369ee5
Sumit Arora <SArora1@indigo.ca>
2018-04-24 17:08:50 -0400

    Changed calculation logic
    Changed calculation logic

--- DynamicProgramming/MinStepsToOneByReducing/MinStepsToOneByReducing/Program.cs
+++ DynamicProgramming/MinStepsToOneByReducing/MinStepsToOneByReducing/Program.cs
@@ -11,8 +11,13 @@ namespace MinStepsToOneByReducing
         static void Main(string[] args)
         {
             DateTime start = DateTime.Now;
-            int target = 4;
-            int[] preCalculatedResults = new int[target];
+            int target = 300;
+            int arrayLength = target + 1;
+            int[] preCalculatedResults = new int[arrayLength];
+            preCalculatedResults[0] = 1;
+            preCalculatedResults[1] = 1;
+            preCalculatedResults[2] = 1;
+            preCalculatedResults[3] = 1;
             Console.WriteLine(CalculateMinStepsDP(target, preCalculatedResults));
             DateTime end = DateTime.Now;
             Console.WriteLine(end.Subtract(start).Milliseconds.ToString());
@@ -22,30 +27,27 @@ namespace MinStepsToOneByReducing
         /// <summary>
         /// 1 + min( F(n-1), F(n/2), F (n/3)  )
         /// </summary>
-        /// <param name="target"></param>
+        /// <param name="arrayLength"></param>
         /// <returns></returns>
-        static int CalculateMinStepsDP(int target, int[] preCalculatedResults)
+        static int CalculateMinStepsDP(int arrayLength, int[] preCalculatedResults)
         {
-            if (target < 0)
+            if (arrayLength < 0)
             {
                 Console.WriteLine("Values lesser than 0 are not supported");
                 return 0;
             }
-            if (target <= 3)
+            if (arrayLength <= 3)
                 return 1;
-            
-            if (preCalculatedResults[target-1] != 0)
-                return preCalculatedResults[target-1];
-            preCalculatedResults[0] = 1;
-            preCalculatedResults[1] = 1;
-            preCalculatedResults[2] = 1;
-            preCalculatedResults[3] = 1;
-            for (int i = 4; i < target; i++)
+
+            if (preCalculatedResults[arrayLength] != 0)
+                return preCalculatedResults[arrayLength];
+
+            for (int i = 4; i < arrayLength + 1; i++)
             {
-                preCalculatedResults[i] = 1 + Math.Min(CalculateMinStepsDP(i - 1, preCalculatedResults), Math.Min(i % 2 == 0 ? (CalculateMinStepsDP(i / 2 , preCalculatedResults)) : int.MaxValue, 
-                                                                                            i%3 == 0 ? CalculateMinStepsDP(i / 3, preCalculatedResults): int.MaxValue));
+                preCalculatedResults[i] = 1 + Math.Min(CalculateMinStepsDP(i - 1, preCalculatedResults), Math.Min(i % 2 == 0 ? (CalculateMinStepsDP(i / 2, preCalculatedResults)) : int.MaxValue,
+                                                                                            i % 3 == 0 ? CalculateMinStepsDP(i / 3, preCalculatedResults) : int.MaxValue));
             }
-            return preCalculatedResults[target - 1];
+            return preCalculatedResults[arrayLength];
         }
     }
 }
