0aeaaf841c0e643c902d0ada92c0644695a8e235
Sumit Arora <SArora1@indigo.ca>
2018-05-02 16:52:41 -0400

    Wrong solution

---
+++ DynamicProgramming/RecursiveSumOfNestedArray/RecursiveSumOfNestedArray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursiveSumOfNestedArray
+{
+    class Program
+    {
+        static int[] array = { 10, 20, 30 };
+        static void Main(string[] args)
+        {
+            int x = SumArray(array, array.Length);
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+
+        static int SumArray(int[] array, int length)
+        {
+            if (length <= 1)
+                return array[0];
+            return array[length - 1] + SumArray(array, length - 1);
+        }
+    }
+}
